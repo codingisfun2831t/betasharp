e455c19217906a41cbe18d00980074c904b8bf0f
ilikecoding-197 <64pigpig2014@gmail.com>
2026-03-25 16:11:03 -0400

    switch debug component saving to nbt, for custom options

--- BetaSharp.Client/Debug/DebugComponent.cs
+++ BetaSharp.Client/Debug/DebugComponent.cs
@@ -1,3 +1,5 @@
+using BetaSharp.NBT;
+
 namespace BetaSharp.Client.Debug;
 
 public abstract class DebugComponent
@@ -5,4 +7,21 @@ public abstract class DebugComponent
     public bool Right { get; set; }
     public abstract IEnumerable<DebugRowData> GetRows(DebugContext ctx);
     public abstract DebugComponent Duplicate();
+
+    public void write(NBTTagCompound nbt)
+    {
+        nbt.SetBoolean("Right", Right);
+
+        writeNBT(nbt);
+    }
+
+    public void read(NBTTagCompound nbt)
+    {
+        Right = nbt.GetBoolean("Right");
+
+        readNBT(nbt);
+    }
+
+    public virtual void writeNBT(NBTTagCompound nbt) { }
+    public virtual void readNBT(NBTTagCompound nbt) { }
 }
